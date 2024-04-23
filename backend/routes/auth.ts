e321a673ebcbfe3cdd82d318cb4c9999e6c33f38
DhrumitPanchal <dhrumitpanchal789@gmail.com>
2024-04-23 16:43:45 +0530

    deleted backend

--- backend/routes/auth.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import AuthController from "../controllers/auth.js";
-
-const authRouter = Router();
-
-authRouter.post("/signup", AuthController.signup);
-authRouter.post("/login", AuthController.login);
-
-export default authRouter;
--- backend/routes/work.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import AuthController from "../controllers/work.js";
-
-const workRouter = Router();
-
-workRouter.post("/add", AuthController.add);
-workRouter.get("/", AuthController.get);
-
-export default workRouter;
